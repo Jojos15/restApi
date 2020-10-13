cfb75f84d02e93600ffb482faec7253a15577d83
jojos15 <georgekosmidis1@yahoo.gr>
2020-10-13 18:25:47 +0300

    bcrypt -> bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const loginVal = require('../middlewares/LogIn');
 const registerVal = require('../middlewares/Register');
